792f9fdbf4efed37882e0cf4639ee9e471e29608
Maksym Voznyi <mak7kas02@gmail.com>
2024-05-10 18:39:21 +0300

    [+] startup and program base configuration

---
+++ ApplianceX.Client.Main/Startup.cs
@@ -0,0 +1,50 @@
+﻿namespace ApplianceX.Client.Main;
+
+public class Startup
+{
+    public IConfiguration Configuration { get; }
+
+
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllersWithViews();
+        services.AddRazorPages();
+    }
+
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Home/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        // app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.UseEndpoints(
+            endpoints =>
+            {
+                endpoints.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
+            }
+        );
+    }
+}
