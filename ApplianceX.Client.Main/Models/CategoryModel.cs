5d71c176a085540db3386e0a45ed3f5b7cff698f
Alexander <vsany@meta.ua>
2024-06-17 01:01:24 +0300

    [+] category model

---
+++ ApplianceX.Client.Main/Models/CategoryModel.cs
@@ -0,0 +1,8 @@
+namespace ApplianceX.Client.Main.Models;
+
+public class CategoryModel
+{
+    // public int Id { get; set; }
+    public string Title { get; set; }
+    public string Cover { get; set; }
+}
